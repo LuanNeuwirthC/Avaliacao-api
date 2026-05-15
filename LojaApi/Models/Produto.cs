c54ed7fd8aa7886a84c57d3ded8ba81d75738099
LuanNeuwirthC <luanneuwirth09@gmail>
2026-05-15 20:01:27 -0300

    final commit

---
+++ LojaApi/Models/Produto.cs
@@ -0,0 +1,11 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace LojaApi.Models {
+    public class Produto {
+        [Key]
+        public int Codigo { get; set; }
+        public string Nome { get; set; } = string.Empty;
+        public string Descricao { get; set; } = string.Empty;
+        public decimal Preco { get; set; }
+    }
+}
